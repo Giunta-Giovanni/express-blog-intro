2412413a6c5dece65728748eb609407f3fab7269
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-04 15:39:24 +0100

    [feat] setting up the server

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ const app = express();
 // specifichiamo la porta su cui il server express sarà in ascolto
 const port = 3000;
 
+// definiamo la prima rotta
+app.get('/', (req,res) => {
+    res.send ('Server del mio blog')
+});
 
+// avviamo il server, mettendolo in ascolto sulla porta indicata
+app.listen (port,() => {
+    console.log (`Example aoo listening on port ${port}`)
+})
