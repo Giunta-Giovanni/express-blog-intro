81624926898db57501a76c10e73983512ab4f767
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-04 15:35:24 +0100

    [feat] Initializing Express

---
+++ app.js
@@ -0,0 +1,8 @@
+// Importiamo express
+const express = require('express');
+// inizializziamo express invocandolo come una funzione e lo salviamo in una variabile
+const app = express();
+// specifichiamo la porta su cui il server express sarà in ascolto
+const port = 3000;
+
+
